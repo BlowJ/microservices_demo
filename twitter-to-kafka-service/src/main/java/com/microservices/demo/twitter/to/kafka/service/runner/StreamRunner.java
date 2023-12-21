3f56cf005d51167e67f5b297a8935b221674e1bc
longph4 <longph4@bidv.com.vn>
2023-12-21 18:10:28 +0700

    add TwitterKafkaStatusListener.java and TwitterKafkaStreamRunner.java

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/StreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/StreamRunner.java
@@ -1,7 +1,9 @@
 package com.microservices.demo.twitter.to.kafka.service.runner;
 
+import org.springframework.stereotype.Component;
 import twitter4j.TwitterException;
 
+@Component
 public interface StreamRunner {
     void start() throws TwitterException;
 }
