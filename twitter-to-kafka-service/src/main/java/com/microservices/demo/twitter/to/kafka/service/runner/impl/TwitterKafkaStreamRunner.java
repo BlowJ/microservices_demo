eaa3cdad9124750c61956eed73f03de87e11f258
longph4 <longph4@bidv.com.vn>
2023-12-22 18:29:16 +0700

    fix @Component TwitterKafkaStatusListener.java and TwitterKafkaStreamRunner.java

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
@@ -5,6 +5,7 @@ import com.microservices.demo.twitter.to.kafka.service.listener.TwitterKafkaStat
 import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import twitter4j.FilterQuery;
 import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
@@ -12,6 +13,7 @@ import twitter4j.TwitterStreamFactory;
 
 import javax.annotation.PreDestroy;
 
+@Component
 public class TwitterKafkaStreamRunner implements StreamRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaStreamRunner.class);
